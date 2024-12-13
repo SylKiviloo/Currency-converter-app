19746bb18a29c3f1b9ed86b45b09d95bc7856c4a
SylKiviloo <syl-k@hotmail.com>
2024-12-13 11:31:36 -0800

    try to get working files back

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Layout from './Layout';
 import Home from './Home';
 import CurrencyConverter from './CurrencyConverter';
+//import ExchangeRates from './ExchangeRates';
 import './App.css';
 
 
 const App = () => {
   return (
-    <Router basename="/Currency-converter-app">
+    <Router>
     <Layout>
       <Switch>
         <Route path="/" exact component={Home} />
